a44a250b0661d2ae6147febd517fa374ec6c3fd0
Unknown <j.murray567@gmail.com>
2020-10-29 11:55:55 +0000

    Removed Access Token

--- tweet.py
+++ tweet.py
@@ -3,10 +3,10 @@ import tweepy
 
 def status(message):
     # personal details
-    consumer_key = "0eA3X2uuhNrss1YXTHHxde1qg"
-    consumer_secret = "frcj79NinQOBGyhfhwMHNGGapD401JHyuQEAqo19B284ksk5ek"
-    access_token = "845034311581601796-RoXubf5sNCxYkb4Wgb2gxZv6hfagycp"
-    access_token_secret = "NUe3foY5ANduJMGILjlIT64NP1XF53TqhphO6I0V2l73N"
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
